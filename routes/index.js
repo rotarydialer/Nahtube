78967c793e194fd96bf0928726283ff71a462127
Christopher Drew <github@rotarydial.com>
2017-11-10 00:31:20 -0500

    Postgres pool connection config

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+const { Pool } = require('pg');
+const pool = new Pool({             // TODO: remove hardcoding
+    user: 'postgres',
+    host: 'localhost',
+    database: 'nahdb',
+    password: 'ohsosecureartthou',
+    port: 5432,
+  });
+  global.pgpool = pool;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'NahTube' });
 });
+
 
 module.exports = router;
