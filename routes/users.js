1f4ef96288b702ea115fd982d525fc6cf43eaf8c
Christopher Drew <github@rotarydial.com>
2017-11-12 00:32:13 -0500

    /users/:username.json route

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   
   (async () => {
       const { rows } = await pgpool.query(`
-              SELECT username, password, common_name, roles 
+              SELECT username, common_name, roles 
               FROM nahtube.users`);
 
       return res.send(JSON.stringify(rows));
@@ -16,6 +16,35 @@ router.get('/', function(req, res, next) {
     //throw e 
     res.status(500);
     return res.send('There was an error.');
+  } ))
+
+
+});
+
+router.get('/:username.json', function(req, res, next) {
+ 
+  (async () => {
+      const { rows } = await pgpool.query(`
+              SELECT username, common_name, roles 
+              FROM nahtube.users
+              WHERE username = $1
+              LIMIT 1;`,[req.params.username]);
+
+      if (rows.length) {
+        console.log('Returning details for user "' + [req.params.username] + '".');
+        return res.send(JSON.stringify(rows[0]));
+      } else {
+        console.log('BAD REQUEST for user "' + [req.params.username] + '".');
+        res.status(404);
+        return res.send('No such user found.');
+      }
+
+              
+  })().catch(e => setImmediate(() => { 
+    //throw e 
+    res.status(500);
+    console.log(e);
+    return res.send('Error: ' + e.message);
   } ))
 
 
