26dd167ff521e4c36f8a09ee8b0395998cf25ff6
ADDad <add.twitch@rotarydial.com>
2017-11-13 00:58:11 -0500

    Add routes for /channels

---
+++ routes/channels.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  console.log('Allowed channels, you say?');
+  
+  (async () => {
+      const { rows } = await pgpool.query(`
+              SELECT id, channel_id, channel_name, channel_data 
+              FROM nahtube.channels_allowed`);
+
+      return res.send(JSON.stringify(rows));
+              
+  })().catch(e => setImmediate(() => { 
+    //throw e 
+    res.status(500);
+    return res.send('There was an error.');
+  } ))
+
+
+});
+
+module.exports = router;
