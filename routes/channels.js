2a5b1498b264e40a4c0634d3106e546f26a3a885
ADDad <add.twitch@rotarydial.com>
2017-11-13 01:03:06 -0500

    Fix that copy pasta comment

--- routes/channels.js
+++ routes/channels.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET list of allowed channels */
 router.get('/', function(req, res, next) {
   console.log('Allowed channels, you say?');
   
