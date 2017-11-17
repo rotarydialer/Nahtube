225ff55539c8c6d66a4a5fdef9c6750bee3a051c
Christopher Drew <github@rotarydial.com>
2017-11-16 22:03:56 -0500

    Add file for YouTube specific routes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var channels = require('./routes/channels');
+var youtube = require('./routes/youtube');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/channels', channels);
+app.use('/youtube', youtube);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
