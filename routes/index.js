562d774e737d104725d234a4c848b4fd6ad824d2
Christopher Drew <github@rotarydial.com>
2017-12-03 02:18:21 -0500

    Working sessions; /checksession route

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var session = require('express-session');
 var router = express.Router();
 
 const { Pool } = require('pg');
@@ -10,6 +11,26 @@ global.pgpool = pool;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('dashboard', { title: 'NahTube' });
+});
+
+router.get('/checksession', function(req, res, next) {
+  if (!req.session) {
+    res.status(401);
+    return res.send('No session!');
+  } else {
+    console.log('session id: %s', req.session.id);
+  }
+
+  if (!req.session.user) {
+    console.log('Session found, but user not logged in');
+    res.status(401);
+    return res.send('Found a session, but you\'re not logged in.');
+  } else {
+    console.log('User #%d logged in: "%s"', req.session.user.id, req.session.user.username);
+    return res.send('You are logged in as "' + req.session.user.username + '".');
+  }
+
   res.render('dashboard', { title: 'NahTube' });
 });
 
@@ -31,6 +52,8 @@ router.post('/login', function(req, res, next) {
 
     if (rows.length) {
       console.log('login successful for "%s".', username);
+      req.session.user = rows[0];
+      console.log(' --> id: %d, name: %s ', req.session.user.id, req.session.user.username);
       return res.status(200).send();
     } else {
       console.log('login NOT AUTHORIZED: "%s"', username);
