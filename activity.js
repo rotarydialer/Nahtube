93c86836a7c191bc7691039e2e240950d67367a4
Christopher Drew <github@rotarydial.com>
2017-12-11 00:50:29 -0500

    [WIP] Tracking more details; still just logs

--- activity.js
+++ activity.js
@@ -1,7 +1,10 @@
 module.exports = {
-    track: function (action, userId) {
+    track: function (action, userId, channelId, details) {
+        console.log('TRACK');
         console.log(' ├─> action: "%s"', action);
         console.log(' └─> userId: "%s"', userId);
+        channelId ? console.log(' └─> channelId: "%s"', channelId) : '';
+        details ? console.log(' └─> details: "%s"', details) : '';
     }
 };
 
