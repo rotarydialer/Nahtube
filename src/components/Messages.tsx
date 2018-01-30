4a7f816c5d37e10a2400e05cf7e6ac845a372250
ADDad <add.twitch@rotarydial.com>
2018-01-29 23:33:53 -0500

    [WIP] Messages progress...

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -8,18 +8,40 @@ export interface MessagesState {
     username: string;
 }
 
-export class Messages extends React.Component<MessagesProps, {}> {
+export class Messages extends React.Component<MessagesProps, MessagesState> {
+    constructor (props: MessagesProps) {
+        super(props);
+
+        this.state = {
+            username: undefined
+        }
+
+    }
+
     render() {
+        const {
+            username
+        } = this.state;
+
+        if (!username || username) return <div>Not logged in.</div>;
+
         return ( 
-            <div>
-                <h4>List and send messages.</h4>
-
-                <p>Mock up?</p>
-
-                
-
-            {/* <MessageViewer /> */}
-            </div>
+                <div className="col-md-4">
+                    <div className="card mb-4 box-shadow">
+                        <img className="card-img-top" src="" data-holder-rendered="true" />
+                        <div className="card-body">
+                            <p className="card-text">This is a REACT video message from another user.</p>
+                            <div className="avatar-message"><img src="/images/avatars/<%= username %>-avatar-sm.png" /></div>
+                        <div className="d-flex justify-content-between align-items-center">
+                            <div className="btn-group">
+                            <button type="button" className="btn btn-sm btn-outline-secondary">Reply</button>
+                            <button type="button" className="btn btn-sm btn-outline-secondary">Delete</button>
+                            </div>
+                            <small className="text-muted">9 mins</small>
+                        </div>
+                        </div>
+                    </div>
+                </div>
         )
     }
 }
