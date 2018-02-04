e4ea37fe310bc6af7f100a6316d49a8c112b34d7
ADDad <add.twitch@rotarydial.com>
2018-02-03 21:44:46 -0500

    React state correctly being set

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Axios from 'axios';
 //import MessageViewer from "./MessageViewer";
 
 export interface MessagesProps { 
@@ -6,6 +7,7 @@ export interface MessagesProps {
 
 export interface MessagesState {
     username: string;
+    commonName: string;
 }
 
 export class Messages extends React.Component<MessagesProps, MessagesState> {
@@ -13,8 +15,30 @@ export class Messages extends React.Component<MessagesProps, MessagesState> {
         super(props);
 
         this.state = {
-            username: undefined
+            username: undefined,
+            commonName: undefined
         }
+    }
+
+    componentDidMount () {
+        if (!this.state.username) {
+            Axios.get('/checksession')
+            .then(
+                (resp) => {
+                    console.log('Gotted a resp[opnsid');
+                    console.log(resp);
+
+                    this.setState({
+                        username: resp.data.username,
+                        commonName: resp.data.common_name
+                    });
+                }
+            )
+            .catch((err) => {
+                console.log('ERROR: ' + err);
+            });
+        }
+
     }
 
     render() {
