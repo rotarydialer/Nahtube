f5eddfcb6172df1ef6c551b8b62714fcbc0e25cc
ADDad <add.twitch@rotarydial.com>
2018-02-09 00:36:45 -0500

    [WIP] New Message

--- src/components/NewMessage.tsx
+++ src/components/NewMessage.tsx
@@ -17,8 +17,28 @@ export default class NewMessage extends React.Component<MessageProps, MessageSta
     render() {
         return (
             <div className="newmessage col">
-                <h2>New Message</h2>
-                <p>And stuff will go here. Have a border.</p>
+                <h2>Send a Message</h2>
+
+                <form>
+                    <div className="form-group">
+
+                        <div className="form-group row">
+                            <label className="col-1 col-form-label">To</label>
+                            <div className="col-11">
+                                <input type="email" className="form-control" id="messageTo" aria-describedby="toHelp" placeholder="Username" />
+                                <small id="toHelp" className="form-text text-muted">Who would you like to send this message to?</small>
+                            </div>
+                        </div>
+                        <div className="form-group row">
+                            <label className="col-1 col-form-label">Subject</label>
+                            <div className="col-11">
+                                <input className="form-control" type="text" id="subject" />
+                            </div>
+                        </div>
+                    
+                        <button type="submit" className="btn btn-primary">Send</button>
+                    </div>
+                </form>
             </div>
         )
 
