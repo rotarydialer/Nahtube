b769338f6c4c9daf22a084107be6917b903d6049
ADDad <add.twitch@rotarydial.com>
2018-02-12 01:02:03 -0500

    Click search result to add video to state

--- src/components/NewMessage.tsx
+++ src/components/NewMessage.tsx
@@ -12,6 +12,10 @@ export interface MessageState {
     searchResults: string[];
     users: string[];
     sendToUsername: string;
+    subject: string;
+    messageBody: string;
+    videoId: string;
+    detailsFull: {}
 }
 
 // TODO: remove these hamfisted crutches in favor of something more elegant
@@ -40,16 +44,21 @@ export default class NewMessage extends React.Component<MessageProps, MessageSta
             searchString: undefined,
             searchResults: [],
             users: [],
-            sendToUsername: undefined
+            sendToUsername: undefined,
+            subject: undefined,
+            messageBody: undefined,
+            videoId: undefined,
+            detailsFull: {}
         }
 
         this.onSearchChange = this.onSearchChange.bind(this);
+        this.selectVideoToSend = this.selectVideoToSend.bind(this);
         this.onSelectUser = this.onSelectUser.bind(this);
+        this.onChangeSubject = this.onChangeSubject.bind(this);
+        this.onChangeBody = this.onChangeBody.bind(this);
     }
 
     componentDidMount () {
-        console.log('New Message component mounted.');
-
         Axios.get('/users')
         .then(
             (userData) => {
@@ -79,7 +88,6 @@ export default class NewMessage extends React.Component<MessageProps, MessageSta
 
         // wait 2 seconds before submitting the search
         setTimeout(() => this.checkSearchTerms(this.state.searchString), 2000);
-
     }
 
     checkSearchTerms(incoming) {
@@ -105,9 +113,9 @@ export default class NewMessage extends React.Component<MessageProps, MessageSta
                 (resultsFound) => {
                     console.log(resultsFound.data);
                     let searchResults = resultsFound.data.items.map( (result) => 
-
-                        <div key={result.id.videoId} data-detailsFull={result} className="col searchResultThumb"><img className="searchResultThumb" src={formatVideoThumbnail(result)} /></div>
-                                                    
+                        // <div key={result.id.videoId} className="col searchResultThumb" onClick={this.selectVideoToSend.bind(this)}><img data-videoid={result.id.videoId} data-fulldetails={result} className="searchResultThumb" src={formatVideoThumbnail(result)} /></div>
+                        // ideally, I should probably serialize and store the full details result above, but in the interest of time I'll just grab the videoId and do a separate request on send :/
+                        <div key={result.id.videoId} className="col searchResultThumb"><img  onClick={this.selectVideoToSend} data-videoid={result.id.videoId} className="searchResultThumb" src={formatVideoThumbnail(result)} /></div>
                     )
 
                     this.setState({searchResults: searchResults});
@@ -124,10 +132,32 @@ export default class NewMessage extends React.Component<MessageProps, MessageSta
         //setTimeout(() => currentSearchExecuted = false, 1000); // clear this flag to allow searches to happen again
     }
 
+    selectVideoToSend(e) {
+        let selectedVideoId = e.target.dataset.videoid;
+        if (selectedVideoId) {
+            this.setState({videoId: selectedVideoId});
+        } else {
+            console.log('ERROR: No video ID found for the selected video.');
+        }
+    }
+
     onSelectUser(selectedUser) {
         this.setState({
             sendToUsername: selectedUser.target.value
         });
+    }
+
+    onChangeSubject(e) {
+        this.setState({[e.target.id]: e.target.value});
+    }
+
+    onChangeBody(e) {
+        this.setState({[e.target.id]: e.target.value});
+    }
+
+    sendMessage(e) {
+        console.log('Preparing to send the message:');
+        console.log(this.state);
     }
 
     render() {
@@ -175,17 +205,17 @@ export default class NewMessage extends React.Component<MessageProps, MessageSta
                             <label className="col-1 col-form-label">Subject</label>
 
                             <div className="col-11">
-                                <input className="form-control" type="text" id="subject" />
+                                <input className="form-control" type="text" id="subject" onChange={this.onChangeSubject} />
                             </div>
                         </div>
 
                         <div className="form-group row">
                             <div className="col-12">
-                                <textarea className="form-control" id="messageBody" rows={5}></textarea>
+                                <textarea className="form-control" id="messageBody" rows={5} onChange={this.onChangeBody}></textarea>
                             </div>
                         </div>
                     
-                        <button type="submit" className="btn btn-primary">Send</button> <button className="btn btn-secondary">Cancel</button>
+                        <button type="submit" className="btn btn-primary" onClick={this.sendMessage}>Send</button> <button className="btn btn-secondary">Cancel</button>
                     </div>
                 </form>
             </div>
