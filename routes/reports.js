79c7f51ea7901e6172d4347145e7bf23026fb3c2
ADDad <add.twitch@rotarydial.com>
2018-02-28 00:47:47 -0500

    feat: report: all activity summary

--- routes/reports.js
+++ routes/reports.js
@@ -2,7 +2,42 @@ var express = require('express');
 var router = express.Router();
 var moment = require('moment');
 
-router.get('/user/videos/watched/date/:dateToCheck?', function (req, res, next) {
+router.get("/user/summary/activity/date/:dateToCheck?", function(req, res, next) {
+  var checkDate = moment(req.params.dateToCheck || Date.now());
+
+  (async () => {
+    qresult = await pgpool.query(
+      `
+            SELECT users.common_name, act.action, count(act.id)
+              FROM nahtube.users users
+              LEFT JOIN nahtube.user_activity act
+                ON act.user_id = users.id
+             WHERE date_trunc('day', act.action_time)::date = $1
+             GROUP BY users.common_name, act.action
+             ORDER BY users.common_name, act.action;`,
+      [checkDate]
+    );
+
+    var { rows } = qresult;
+
+    if (rows.length) {
+      rows.push();
+      return res.send(rows);
+    } else {
+      res.status(404);
+      return res.send("No data found for the given date.");
+    }
+  })().catch(e =>
+    setImmediate(() => {
+      //throw e
+      res.status(500);
+      console.log(e);
+      return res.send("Error: " + e.message);
+    })
+  );
+});
+
+router.get('/user/summary/videos/watched/date/:dateToCheck?', function (req, res, next) {
     
     var checkDate = moment(req.params.dateToCheck || Date.now());
 
