1118e359fde7612165246310b4f4c29518c66b1f
ADDad <add.twitch@rotarydial.com>
2018-03-01 00:24:58 -0500

    Daily report shows all users

--- routes/reports.js
+++ routes/reports.js
@@ -20,9 +20,12 @@ router.get("/user/summary/activity/date/:dateToCheck?", function(req, res, next)
 
     var { rows } = qresult;
 
+    var data = {};
+    data.results = rows;
+
     if (rows.length) {
       rows.push();
-      return res.send(rows);
+      return res.send(data);
     } else {
       res.status(404);
       return res.send("No data found for the given date.");
@@ -43,19 +46,34 @@ router.get('/user/summary/videos/watched/date/:dateToCheck?', function (req, res
 
     (async () => {
         qresult = await pgpool.query(`
-            SELECT users.common_name, date_trunc('day', act.action_time)::date as date, count(act.id)
+          WITH all_users AS (
+            SELECT common_name, id FROM nahtube.users WHERE is_active
+          ),
+          
+          activity_by_day AS (
+            SELECT users.common_name, users.id, date_trunc('day', act.action_time)::date as date, count(act.id) as count
               FROM nahtube.users users
               LEFT JOIN nahtube.user_activity act
                 ON act.user_id = users.id
-             WHERE action = 'watch video'
-               AND date_trunc('day', act.action_time)::date = $1
-             GROUP BY users.common_name, date;`,
-            [checkDate]);
+            WHERE action = 'watch video'
+              AND date_trunc('day', act.action_time)::date = $1
+            GROUP BY users.common_name, users.id, date
+          )
+
+          SELECT u.common_name, COALESCE(a.count, 0) as count from all_users u
+          LEFT OUTER JOIN activity_by_day a
+          ON u.id = a.id
+          ORDER BY a.count DESC NULLS LAST, u.common_name;`,
+          [checkDate]);
 
         var { rows } = qresult;
+
+        var data = {};
+        data.date = checkDate;
+        data.results = rows;
   
         if (rows.length) {
-          return res.send(rows);
+          return res.send(data);
         } else {
           res.status(404);
           return res.send('No data found for the given date.');
