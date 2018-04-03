efb0f87ae5c6272d0bfcb0804c996aea193f0b33
Christopher Drew <Christopher_Drew@s5a.com>
2018-04-03 13:30:09 -0400

    feat: User Management [WIP]

---
+++ src/components/admin/UserManagement.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import * as Moment from "moment";
+
+import UserSelector from "../UserSelector";
+
+export interface Props {
+}
+
+export interface State {
+    selectedUser: string;
+}
+
+export class UserManagement extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            selectedUser: undefined
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                User Management
+
+                <UserSelector defaultUser=''/>
+            </div>
+        )
+    }
+}
