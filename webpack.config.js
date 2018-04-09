aaa3acb6a207d2e1f2c93b2091da6d2d1484ed7b
Christopher Drew <Christopher_Drew@s5a.com>
2018-04-09 12:06:32 -0400

    chore: remove obsolete user admin

--- webpack.config.js
+++ webpack.config.js
@@ -66,30 +66,5 @@ module.exports = [
             "react": "React",
             "react-dom": "ReactDOM"
         },
-    },
-    {
-        entry: "./src/admin.js",
-        output: {
-            filename: "useradmin.bundle.js",
-            path: __dirname + "/dist"
-        },
-        devtool: "source-map",
-        resolve: {
-            extensions: [".js", ".json"]
-        },
-        module: {
-            rules: [
-                { 
-                    test: /\.(js|jsx)?$/, 
-                    loader: 'babel-loader',
-                    exclude: /node_modules/ 
-                },
-                { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-            ]
-        },
-        externals: {
-            "react": "React",
-            "react-dom": "ReactDOM"
-        },
     }
 ];
