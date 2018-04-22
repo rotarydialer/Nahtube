65b5c098e9b3845dcdecf3dcea3902632d7898d6
ADDad <add.twitch@rotarydial.com>
2018-04-22 01:11:32 -0400

    docs: helpful user report/count queries

--- database/q_activity.sql
+++ database/q_activity.sql
@@ -75,3 +75,47 @@ FROM nahtube.user_messages msg
 WHERE users_to.username = 'daughter'
 AND msg.message_time >= DATE(NOW()) - INTERVAL '1 DAY' * 7
 ORDER BY msg.message_time DESC;
+
+-- report: daily watch counts per user, per day
+WITH now_tz AS (
+	select (date_trunc('day', now() AT TIME ZONE '+04')) AS todays_date
+),
+
+all_users AS (
+	SELECT common_name, id, username FROM nahtube.users WHERE is_active
+),
+          
+date_range AS (
+        SELECT 
+        generate_series('04-11-2018', 
+		(SELECT todays_date FROM now_tz), 
+		'1 day'::interval)::date AS arb_date
+      ),
+            
+activity_dates AS (
+	SELECT user_id,
+	       date_trunc('day', a.action_time at time zone '+04') AS action_date,
+	       count(a.action_time) AS watch_count
+	  FROM nahtube.user_activity a
+	 INNER JOIN nahtube.users u
+		 ON a.user_id = u.id
+	 WHERE a.action = 'watch video'
+	 GROUP BY user_id, username, action_date
+	 ORDER BY action_date, username
+)
+
+SELECT ad.arb_date, u.common_name, COALESCE(a.watch_count, 0) as count 
+  FROM date_range ad
+ CROSS JOIN all_users u
+  LEFT JOIN activity_dates a
+         ON (ad.arb_date = a.action_date AND a.user_id = u.id)
+   ORDER BY arb_date, u.id;
+
+-- user "grand" summary: all activity
+SELECT u.common_name, u.id, u.username, a.action, COUNT(a.action_time) as action_total 
+FROM nahtube.users u
+INNER JOIN nahtube.user_activity a
+      ON (u.id = a.user_id)
+WHERE u.is_active
+GROUP BY u.common_name, u.id, u.username, a.action
+ORDER BY u.common_name, u.id, u.username, action_total DESC;
